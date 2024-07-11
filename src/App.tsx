5ff71019a008157c6f88bf8709747898154dc3cf
lillykml <64362067+lillykml@users.noreply.github.com>
2024-07-11 12:41:56 +0200

    Basic Frontend

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,53 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState<number | undefined >(undefined);
+  const [origin, setOrigin] = useState<string | undefined>(undefined);
+  const [target, setTarget] = useState<string | undefined>(undefined);
+  const [result, setResult] = useState<string | undefined>(undefined);
+
+  const convert = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    setResult(`Converting ${amount} ${origin} to ${target}`)
+    setAmount(undefined);
+    setOrigin('');
+    setTarget('');
+  }
 
   return (
     <>
       <div>
         <img src="/icon512.png" className="logo" alt="Currency converter logo" />
+        <button onClick={()=>window.close()}>X</button>
       </div>
       <h1>Currency Converter</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <form onSubmit={convert}>
+          <div>
+            <label>Amount</label>
+            <input 
+            typeof='number' 
+            value={amount} 
+            onChange={(e) => {
+              const value = e.target.value;
+              setAmount(value ? parseFloat(value) : undefined);
+              }
+            }/>
+          </div>
+          <div>
+            <label>Original Currency</label>
+            <input type='text' value={origin} onChange={(e) => setOrigin(e.target.value)}/>
+          </div>
+          <div>
+            <label>Target Currency</label>
+            <input type='text' value={target} onChange={(e) => setTarget(e.target.value)}/>
+          </div>
+          <button type='submit'>Convert</button>
+        </form>
+        {result && <p>{result}</p>}
       </div>
     </>
   )
 }
 
-export default App
+export default App;
